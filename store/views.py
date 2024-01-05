7b4fcd996c85c362c1960f011f32977d85fffdf3
Stelios Alevizos <stelios.alevizos@protonmail.com>
2024-01-05 18:14:08 +0200

    Category model and link list functionality
    new model Category, new url category, new view category (it takes an https query and makes a query to django to fetch products with selected category)

--- store/views.py
+++ store/views.py
@@ -4,14 +4,23 @@ from .models import *
 # Create your views here.
 
 def cart(request):
-    context={}
+    context = {}
     return render(request, 'store/cart.html', context)
 
 def store(request):
     products = Product.objects.all()
-    context={'products' : products}
+    categories = Category.objects.all()
+    context = {'products' : products, 'categories' : categories}
     return render(request, 'store/store.html', context)
 
 def checkout(request):
-    context={}
+    context = {}
     return render(request, 'store/checkout.html', context)
+
+def category(request):
+    choice = request.GET.get('choice', '')
+    id = Category.objects.get(name=choice)
+    products = Product.objects.filter(category_id=id)
+    categories = Category.objects.all()
+    context = {'products' : products, 'categories' : categories}
+    return render(request, 'store/store.html', context)
