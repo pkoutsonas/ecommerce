a97666b74db964d5f3d1ce1d0aa159d5c67344cd
Stelios Alevizos <stelios.alevizos@protonmail.com>
2024-01-07 20:58:45 +0200

    Register Login WIP

--- store/urls.py
+++ store/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
@@ -7,4 +7,6 @@ urlpatterns = [
     path('checkout/', views.checkout, name = 'checkout'),
     path('category/', views.category, name = 'category'),
     path('search/', views.search, name = 'search'),
+    path('login/', views.login, name = 'login'),
+    path('register/', views.register, name = 'register'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,46 +1,82 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate
 from .models import *
+from .forms import *
 
-
-def check_cookie_session(request):
+# Return session variable 'cookie_var' 
+def update_cookie_session(request):
     cookie_var = request.session.get('cookie_var', None)
 
     if cookie_var is None:
-        request.session['cookie_var'] = 1
-        return 1
-    else:
-        request.session['cookie_var'] = cookie_var + 1
-        return cookie_var
+        cookie_var = 1
+    
+    request.session['cookie_var'] = cookie_var + 1
+    return cookie_var
 
+# Returns cookie value and cart instance 
 def cookie_and_cart(request):
     cookie_value = request.COOKIES.get('user_cookie', None)
     if cookie_value is None:
         cart_instance = Cart.objects.create(number_of_items=0)
-        cookie_var = check_cookie_session(request)
+        cookie_var = update_cookie_session(request)
         Guest.objects.create(cart=cart_instance ,cookie=cookie_var)
     else:
         guest_instance = Guest.objects.get(cookie=cookie_value)
         cart_instance = guest_instance.cart        
-    return cookie_var-1, cart_instance
+        cookie_var = cookie_value
 
-# Create your views here.
+    return cookie_var, cart_instance
 
-def cart(request):
-    context={}
-    return render(request, 'store/cart.html', context)
-
-def store(request):
-    products = Product.objects.all()
-    categories = Category.objects.all()
+# build store with provided filtered products/categories
+def build_store_cookie(request, products, categories):
     cookie_value, cart_instance = cookie_and_cart(request)
     context = {'products' : products, 'categories' : categories, 'cart' : cart_instance}
     response = render(request, 'store/store.html', context)
     response.set_cookie('user_cookie', cookie_value)
+
     return response
+
+
+############################### Create your views here. ###############################
+
+def cart(request):
+    context={}
+
+    return render(request, 'store/cart.html', context)
 
 def checkout(request):
     context = {}
+
     return render(request, 'store/checkout.html', context)
+
+
+def login(request):
+    inputUsername = request.POST.get('inputUsername', '')
+    
+    context = {}
+
+    return render(request, 'store/login.html', context)
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return store(request)
+    else:
+        form = RegistrationForm()
+
+    return render(request, 'store/register.html', {'form': form})
+
+# Store template
+
+def store(request):
+    products = Product.objects.all()
+    categories = Category.objects.all()
+
+    return build_store_cookie(request, products, categories)
 
 def category(request):
     q = request.GET.get('q', '')
@@ -48,13 +84,13 @@ def category(request):
 
     products = Product.objects.filter(category_id=id)
     categories = Category.objects.all()
-    context = {'products' : products, 'categories' : categories}
-    return render(request, 'store/store.html', context)
+
+    return build_store_cookie(request, products, categories)
 
 def search(request):
     q = request.GET.get('q', '')
 
     products = Product.objects.filter(name__icontains=q)
     categories = Category.objects.all()
-    context = {'products' : products, 'categories' : categories}
-    return render(request, 'store/store.html', context)
+
+    return build_store_cookie(request, products, categories)
